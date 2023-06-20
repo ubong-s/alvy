b15950b8d0758964d984b8eb28c87c26d42bdcb2
Ubong Sylvester <ubongsly@gmail.com>
2023-06-20 16:19:48 +0100

    footer styling completed

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,3 @@
 export { menu_links } from "./menu_links";
 export { footer_links } from "./footer_links";
+export { social_links } from "./social_links";
---
+++ constants/social_links.ts
@@ -0,0 +1,18 @@
+export const social_links = [
+  {
+    text: "twitter",
+    link: "https://www.twitter.com",
+  },
+  {
+    text: "instagram",
+    link: "https://www.instagram.com",
+  },
+  {
+    text: "behance",
+    link: "https://www.behance.com",
+  },
+  {
+    text: "dribble",
+    link: "https://www.dribble.com",
+  },
+];
