4a6b3d7fb2a6fa9800214c0924143223b2107331
Ubong Sylvester <ubongsly@gmail.com>
2023-08-16 17:39:32 +0100

    completed blog post intro styling

---
+++ app/(routes)/blog/[slug]/components/post-content.tsx
@@ -0,0 +1,3 @@
+export const PostContent = () => {
+  return <div>PostContent</div>;
+};
--- components/ui/subscription-form.tsx
+++ components/ui/subscription-form.tsx
@@ -1,31 +1,34 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export const SubscriptionForm = () => {
   return (
-    <section className="flex flex-col gap-4 rounded-lg bg-cod-gray-2 p-4 lg:flex-row lg:justify-between lg:items-center">
-      <div className="flex items-center gap-8">
+    <section className='flex flex-col gap-4 rounded-lg bg-cod-gray-2 p-4 lg:flex-row lg:justify-between lg:items-center'>
+      <div className='flex items-center gap-8'>
         <Image
-          src="/images/noise.png"
-          alt="sub"
+          src='/images/noise.png'
+          alt='sub'
           width={150}
           height={100}
-          className="rounded-lg "
+          className='rounded-lg '
         />
         <div>
-          <h2 className="font-roboto-mono">NewsLetter</h2>
-          <p className="font-chillax text-lg uppercase lg:text-2xl">
+          <h2 className='font-roboto-mono'>NewsLetter</h2>
+          <p className='font-chillax text-lg uppercase lg:text-2xl'>
             Get Insights behind the scenes
           </p>
         </div>
       </div>
 
-      <form className="flex gap-2">
+      <form className='flex gap-2'>
         <input
-          type="email"
-          placeholder="Your email"
-          className="uppercase font-roboto-mono p-5 bg-transparent text-white rounded-lg border border-t-gray outline-none flex-grow lg:w-[350px]"
+          type='email'
+          placeholder='Your email'
+          className='uppercase font-roboto-mono p-5 bg-transparent text-white rounded-lg border border-t-gray outline-none flex-grow lg:w-[350px]'
         />
-        <button className="p-5 rounded-lg bg-sundance text-cod-gray uppercase font-roboto-mono">
+        <button
+          type='submit'
+          className='p-5 rounded-lg bg-sundance text-cod-gray uppercase font-roboto-mono'
+        >
           Subscribe
         </button>
       </form>
